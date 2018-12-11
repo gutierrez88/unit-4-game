d83dc895c34848532dff59b809acbe1f27627ed2
Ryan <franciscogutierrez@Franciscos-MacBook-Pro.local>
2018-12-11 18:31:18 -0500

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var wins;
+var losses;
+var targetScore;
+var currentScore = 0;
+var picValue =[];
+
+
+for (i=0;i < 4 ;i++) {
+    picValue[i] = Math.floor(Math.random()*12)+1;
+    console.log(picValue);
+}
